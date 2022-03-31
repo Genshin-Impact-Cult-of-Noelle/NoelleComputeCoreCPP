7d0dd1625f4566366478838ded1a15f111afbe7c
YueAo7 <Leaoqi@outlook.com>
2022-04-01 00:29:46 +0800

    ✨ feat(角色基): 所有角色的唯一基类

--- src/lib/Core/Role.cpp
+++ src/lib/Core/Role.cpp
@@ -1,1 +1,12 @@
 ﻿#include "Role.h"
+namespace Core {
+	Role::Role(Attr** attrs, CharacterGender gender, CharacterGroup group, ElementType element, WeaponType weaponType) {
+		Changed = false;
+		rawBuffPool = new BuffPool();
+		rawWeapon = new Weapon();
+		rawBuffPool = new BuffPool();
+		rawCharacter = new Character(attrs, gender, group, element, weaponType);
+
+	};
+
+}
--- src/lib/Core/Role.h
+++ src/lib/Core/Role.h
@@ -1,9 +1,33 @@
 ﻿#pragma once
 #include "Character.h"
+#include "Buff.h"
+#include "Weapon.h"
+#include "Artifact.h"
 namespace Core {
 	class Role {
 	public:
-		Character* rawCharacter;
+		Role(Attr**, CharacterGender, CharacterGroup, ElementType, WeaponType);
+		//添加buff
+		Role* AddBuff(Buff*);
+		//设置武器
+		Role* SetWeapon(Weapon*);
+		//设置圣遗物
+		Role* SetArtifact(Artifact*);
+		//设置圣遗物套装
+		Role* SetArtifactSet();
+		//获取当前面板
+		void GetLastData();
+		//帧更新
+		void Update();
+	private:
+		//数据改变标识
+		bool Changed;
 
+		//内部角色数据
+		Character* rawCharacter;
+		//内部武器数据
+		Weapon* rawWeapon;
+		//内部Buff池
+		BuffPool* rawBuffPool;
 	};
 }
