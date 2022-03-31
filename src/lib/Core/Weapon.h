daa37c7dad5bab87b24f099c1bb67b2e41163c36
YueAo7 <Leaoqi@outlook.com>
2022-03-31 21:06:06 +0800

    ✨ feat(武器类): 创建

---
+++ src/lib/Core/Weapon.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "BaseObject.h"
+#include "Skill.h"
+namespace Core {
+#pragma region 武器类
+
+	class Weapon {
+	public:
+
+		Weapon(Attr**);
+		//void (*skill)(Character*, Character*, uint32_t);
+	};
+#pragma endregion
+}
+
+
