5875426932f4eaac6ddae7a165b782fc5a35f938
YueAo7 <Leaoqi@outlook.com>
2022-03-31 21:03:31 +0800

    ✨ feat(角色基础类): 创建

---
+++ src/lib/Core/Character.cpp
@@ -0,0 +1,11 @@
+﻿#include "Character.h"
+using namespace Core::Enum;
+namespace Core {
+	Character::Character(Attr** Data, CharacterGender Gender, CharacterGroup Group, ElementType Element, WeaponType Weapon) {
+		BaseObject::BaseObject(Data);
+		gender = Gender;
+		group = Group;
+		godEye = Element;
+		weaponType = Weapon;
+	}
+}
---
+++ src/lib/Core/Character.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "BaseObject.h"
+namespace Core {
+
+
+
+#pragma region 人物类
+
+	class Character : BaseObject {
+
+	public:
+
+		//性别
+		Character(Attr**, CharacterGender, CharacterGroup, ElementType, WeaponType);
+
+		//性别
+		CharacterGender gender;
+
+		//势力
+		CharacterGroup group;
+
+		//神之眼
+		ElementType godEye;
+
+		//武器类型
+		WeaponType weaponType;
+
+		//命之座
+		//
+
+		//技能等级
+		//
+
+
+	};
+
+#pragma endregion
+
+
+}
