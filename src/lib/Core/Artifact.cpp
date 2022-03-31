a14d94d8ff16f8214d82caadaaa33c5451e79819
YueAo7 <Leaoqi@outlook.com>
2022-03-31 21:02:51 +0800

    ✨ feat(圣遗物): 圣遗物类创建

---
+++ src/lib/Core/Artifact.cpp
@@ -0,0 +1,17 @@
+﻿#include "Artifact.h"
+namespace Core {
+#pragma region 圣遗物类实现
+
+	Artifact::Artifact(Attr** Data) {
+		BaseObject::BaseObject(Data);
+
+	}
+
+#pragma endregion
+
+#pragma region 圣遗物组类实现
+
+
+
+#pragma endregion
+}
---
+++ src/lib/Core/Artifact.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "BaseObject.h"
+namespace Core {
+#pragma region 圣遗物类
+
+	class Artifact : BaseObject {
+	public:
+		Artifact(Attr**);
+
+	};
+
+#pragma endregion
+
+#pragma region 圣遗物组类
+	class ArtifactGroup : BaseObject {
+	public:
+		ArtifactGroup(Artifact**);
+		void	(*update) (uint32_t);
+
+
+	};
+#pragma endregion
+}
+
